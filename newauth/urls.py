7480bab700adcc5aa0bea067dc000cf656dd2fdd
crohaco <crohaco.net@gmail.com>
2015-02-23 09:40:59 +0900

    modified the import path in urls.py

--- newauth/urls.py
+++ newauth/urls.py
@@ -1,6 +1,9 @@
 #:coding=utf-8:
 
-from django.conf.urls.defaults import patterns, url
+try:
+    from django.conf.urls.defaults import patterns, url
+except ImportError:
+    from django.conf.urls import patterns, url
 
 urlpatterns = patterns('newauth.views',
     url(r'^login/$', 'login', name='newauth_login'),
