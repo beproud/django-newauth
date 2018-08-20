7a7919d503b81a7235539d6f618681c9530352f3
mtb-beta <mtb.toya0403@gmail.com>
2018-08-20 13:43:06 +0900

    updated document

--- newauth/signals.py
+++ newauth/signals.py
@@ -2,5 +2,8 @@
 
 from django.dispatch import Signal
 
+#: user logged in signal
 user_logged_in = Signal(providing_args=['request', 'user'])
+
+#: user logged out signal
 user_logged_out = Signal(providing_args=['request', 'user'])
