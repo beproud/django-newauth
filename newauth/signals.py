529ba62d424d7ea17f484dedc403bd25549793b2
Tatsuya Matoba <mtb.toya0403@gmail.com>
2018-08-21 01:15:25 +0000

    updated document

--- newauth/signals.py
+++ newauth/signals.py
@@ -2,8 +2,8 @@
 
 from django.dispatch import Signal
 
-#: user logged in signal
+#: This is notification by Django signals format, send after a logged in.
 user_logged_in = Signal(providing_args=['request', 'user'])
 
-#: user logged out signal
+#: This is notification by Django signals format, send after a logged out.
 user_logged_out = Signal(providing_args=['request', 'user'])
