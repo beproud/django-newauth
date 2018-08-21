0b366270cfc2f7ebc3b5546f91ccdd0d203da89b
mtb-beta <mtb.toya0403@gmail.com>
2018-08-21 16:29:48 +0900

    update document

--- newauth/signals.py
+++ newauth/signals.py
@@ -3,13 +3,43 @@
 from django.dispatch import Signal
 
 
-#: This is `django.dispatch.Signal` instances, Notification send after a logged in.
-#:
-#: If you don't know `django.dispatch.Signal` see: https://docs.djangoproject.com/ja/1.11/topics/signals/
 user_logged_in = Signal(providing_args=['request', 'user'])
+"""
+This is `django.dispatch.Signal` instances.
+This is sent at the end of the `newauth.api.login()` method.
+
+Arguments sent with this signal:
+
+**sender**
+
+    The class of the user that just logged in.
+
+**request**
+
+    The current `django.http.request.HttpRequest` instance.
+
+**user**
+
+    The user instance that just logged in.
+"""
 
 
-#: This is `django.dispatch.Signal` instances. Notification send after a logged out.
-#:
-#: If you don't know `django.dispatch.Signal` see: https://docs.djangoproject.com/ja/1.11/topics/signals/
 user_logged_out = Signal(providing_args=['request', 'user'])
+"""
+This is `django.dispatch.Signal` instances.
+This is sent at the end of the `newauth.api.logout()` method.
+
+Arguments sent with this signal:
+
+**sender**
+
+    As above: the class of the user that just logged out or None if the user was not authenticated.
+
+**request**
+
+    The current `django.http.request.HttpRequest` instance.
+
+**user**
+
+    The user instance that just logged out or None if the user was not authenticated.
+"""
